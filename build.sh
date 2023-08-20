0d90f1d71c016949b1e9c8e4cffc2a76529475f7
Tamas Jantvik <tsjk@hotmail.com>
2023-08-20 22:08:53 +0200

    Fix of simple build script

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 if [ -z "${1}" ]; then
-  PLATFORM="linux/amd64"
+  PLATFORM="linux/amd64"; TAG_PREFIX="amd64"
 else
   case "${1}" in
-    "linux/arm64")    PLATFORM="linux/arm64"          ;;
-    "linux/arm32v7")  PLATFORM="linux/arm32v7"        ;;
-    "linux/amd64")    PLATFORM="linux/amd64"          ;;
+    "linux/arm64")    PLATFORM="linux/arm64";   TAG_PREFIX="arm64"    ;;
+    "linux/arm32v7")  PLATFORM="linux/arm32v7"; TAG_PREFIX="arm32v7"  ;;
+    "linux/amd64")    PLATFORM="linux/amd64";   TAG_PREFIX="amd64"    ;;
     "all")            PLATFORM="linux/amd64"; \
                       PLATFORM+=",linux/arm64"; \
                       PLATFORM+=",linux/arm32v7"      ;;
@@ -14,4 +14,10 @@ else
 fi
 
 export BUILDAH_FORMAT='docker' && \
-  buildah bud --platform "${PLATFORM}" --pull --layers --manifest local/core-lightning:latest --file Dockerfile .
+  { if echo "${PLATFORM}" | grep -q -E '.*,.*'; then
+      buildah bud --platform "${PLATFORM}" --pull --layers --manifest local/core-lightning:latest --file Dockerfile .
+    else
+      buildah bud --platform "${PLATFORM}" --pull --layers --tag local/core-lightning:${TAG_PREFIX}-latest --file Dockerfile . && \
+      echo "NOTE: Add the new images to a manifest if needed."
+    fi
+  }
