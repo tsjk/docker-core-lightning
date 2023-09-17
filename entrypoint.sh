f761117e582ef5dcf0a8bddee2df4f3366936dee
Tamas Jantvik <tsjk@hotmail.com>
2023-09-17 21:20:06 +0200

    Addition of some more error handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,9 @@
 : "${START_RTL:=true}"
 : "${START_IN_BACKGROUND:=false}"
 
+__warning() {
+  echo "WARNING: ${*}" >&2
+}
 __error() {
   echo "ERROR: ${*}" >&2; exit 1
 }
@@ -36,22 +39,27 @@ fi
 if [[ "${1}" == "${LIGHTNINGD}" ]]; then
   if [[ "${PUID}" =~ ^[0-9][0-9]*$ && "${PGID}" =~ ^[0-9][0-9]*$ ]]; then
     { [[ $(getent group lightning | cut -d ':' -f 3) -eq ${PGID} ]] || gruopmod --non-unique --gid ${PGID} lightning; } && \
-    { [[ $(getent passwd lightning | cut -d ':' -f 3) -eq ${PUID} ]] || usermod --non-unique --uid ${PUID} lightning; } || exit 1; fi
+      { [[ $(getent passwd lightning | cut -d ':' -f 3) -eq ${PUID} ]] || usermod --non-unique --uid ${PUID} lightning; } || \
+      __error "Failed to change uid or gid or \"lightning\" user."; fi
   [[ "${DO_CHOWN}" != "true" ]] || \
-    { [[ -n "${LIGHTNINGD_HOME}" && -d "${LIGHTNINGD_HOME}" ]] && chown -R lightning:lightning "${LIGHTNINGD_HOME}"; } || exit 1
+    { [[ -n "${LIGHTNINGD_HOME}" && -d "${LIGHTNINGD_HOME}" ]] && chown -R lightning:lightning "${LIGHTNINGD_HOME}"; } || \
+      __error "chown -R lightning:lightning \"${LIGHTNINGD_HOME}\" - returned with error"
 
   [[ -z "${NETWORK_RPCD}" ]] || { [[ -e /tmp/socat-network_rpc.lock ]] && [[ -e /tmp/socat-network_rpc.pid ]] && kill -0 `cat /tmp/socat-network_rpc.pid` > /dev/null 2>&1; } || {
       rm -f /tmp/socat-network_rpc.lock /tmp/socat-network_rpc.pid
       su -s /bin/sh lightning -c "exec /usr/bin/socat -L /tmp/socat-network_rpc.lock TCP4-LISTEN:8332,bind=127.0.0.1,reuseaddr,fork TCP4:${NETWORK_RPCD}" &
-      echo $! > /tmp/socat-network_rpc.pid; }
+      echo $! > /tmp/socat-network_rpc.pid
+      kill -0 $(< /tmp/socat-network_rpc.pid) > /dev/null 2>&1 || __error "Failed to setup socat for crypto daemon's rpc service"; }
   [[ -z "${TOR_SOCKSD}" ]] || { [[ -e /tmp/socat-tor_socks.lock ]] && [[ -e /tmp/socat-tor_socks.pid ]] && kill -0 `cat /tmp/socat-tor_socks.pid` > /dev/null 2>&1; } || {
       rm -f /tmp/socat-tor_socks.lock /tmp/socat-tor_socks.pid
       su -s /bin/sh lightning -c "exec /usr/bin/socat -L /tmp/socat-tor_socks.lock TCP4-LISTEN:9050,bind=127.0.0.1,reuseaddr,fork TCP4:${TOR_SOCKSD}" &
-      echo $! > /tmp/socat-tor_socks.pid; }
+      echo $! > /tmp/socat-tor_socks.pid
+      kill -0 $(< /tmp/socat-tor_socks.pid) > /dev/null 2>&1 || __error "Failed to setup socat for Tor SOCKS service"; }
   [[ -z "${TOR_CTRLD}" ]] || { [[ -e /tmp/socat-tor_ctrl.lock ]] && [[ -e /tmp/socat-tor_ctrl.pid ]] && kill -0 `cat /tmp/socat-tor_ctrl.pid` > /dev/null 2>&1; } || {
       rm -f /tmp/socat-tor_ctrl.lock /tmp/socat-tor_ctrl.pid
       su -s /bin/sh lightning -c "exec /usr/bin/socat -L /tmp/socat-tor_ctrl.lock  TCP4-LISTEN:9051,bind=127.0.0.1,reuseaddr,fork TCP4:${TOR_CTRLD}" &
-      echo $! > /tmp/socat-tor_ctrl.pid; }
+      echo $! > /tmp/socat-tor_ctrl.pid
+      kill -0 $(< /tmp/socat-tor_ctrl.pid) > /dev/null 2>&1 || __error "Failed to setup socat for Tor control service"; }
 
   CL_REST_CONFIG_FILE="${LIGHTNINGD_HOME}/.config/c-lightning-REST/cl-rest-config.json"
   if [[ "${START_CL_REST}" == "true" && -s "${CL_REST_CONFIG_FILE}" ]]; then
@@ -123,7 +131,8 @@ if [[ "${1}" == "${LIGHTNINGD}" ]]; then
       if grep -q -E '<RTL-MACAROON-PATH>' "${RTL_CONFIG_FILE}" 2>/dev/null; then
         mkdir "/tmp/RTL-macaroon" && \
           cp -a "${LIGHTNINGD_HOME}/.config/c-lightning-REST/certs/access.macaroon" "/tmp/RTL-macaroon/" && \
-          sed -i 's@<RTL-MACAROON-PATH>@/tmp/RTL-macaroon@' "${RTL_CONFIG_FILE}" || START_RTL="false"
+            sed -i 's@<RTL-MACAROON-PATH>@/tmp/RTL-macaroon@' "${RTL_CONFIG_FILE}" || \
+           { __warning "Failed to set macaroon for RTL. Will hence not start RTL."; START_RTL="false"; }
       fi
       if [[ "${START_RTL}" == "true" ]]; then
         echo "Starting RTL."
