da7a4bd0ba51ff8c5bd133d66c274c1d034da8ae
Tamas Jantvik <tsjk@hotmail.com>
2023-09-26 17:37:41 +0200

    Improved example

--- examples/.post-start.d/01-clboss_ignore_onchain.sh
+++ examples/.post-start.d/01-clboss_ignore_onchain.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-( set -x &&/usr/local/bin/lightning-cli --rpc-file="${LIGHTNINGD_DATA}/${LIGHTNINGD_NETWORK}/lightning-rpc" clboss-ignore-onchain )
+clboss_internet_connection() { /usr/local/bin/lightning-cli --rpc-file=/home/lightning/.lightning/bitcoin/lightning-rpc clboss-status | jq -r '.internet.connection'; }
+
+CLBOSS_STATUS=$(clboss_internet_connection)
+while [ "${CLBOSS_STATUS}" != "offline" -a "${CLBOSS_STATUS}" != "online" ]; do sleep .25; CLBOSS_STATUS=$(clboss_internet_connection); done
+
+( set -x && /usr/local/bin/lightning-cli --rpc-file="${LIGHTNINGD_DATA}/${LIGHTNINGD_NETWORK}/lightning-rpc" clboss-ignore-onchain )
