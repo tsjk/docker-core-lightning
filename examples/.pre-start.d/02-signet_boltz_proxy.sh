0d31a8eabb2da6d1423ba383684b6107ee85a77a
Tamas Jantvik <tsjk@hotmail.com>
2025-01-22 14:09:51 +0100

    Update of example of a socat proxy for signet boltz service
    At the time of writing a proxy is needed for accessing boltz onion
    services in clboss when the lightning node is not configured to always
    use the tor-proxy

--- examples/.pre-start.d/02-signet_boltz_proxy.sh
+++ examples/.pre-start.d/02-signet_boltz_proxy.sh
@@ -3,13 +3,19 @@ __B=$(basename "${BASH_SOURCE[0]}")
 __PIDFILE="/tmp/signet-boltz-proxy.pid"
 __LOGFILE="/tmp/signet-boltz-proxy.log"
 if [ -z "${TOR_SOCKSD}" ]; then
-  printf '%s - INFO: \"TOR_SOCKSD\" environment variable is unset - not starting proxy.\n' "${__B}"
+  printf '%s - INFO: \"TOR_SOCKSD\" environment variable is unset - not starting Boltz proxy.\n' "${__B}"
   exit 0
 fi
+
+curl -LSs -m 15 --socks5-hostname "${TOR_SOCKSD}" -s 'https://check.torproject.org/' 2> /dev/null | \
+  grep -E 'Your IP address appears to be|Congratulations' | uniq | sed 's/<[^>]*>/\n/g' | \
+  xargs | grep -qE 'Your IP address appears to be: [0-9]{1,3}(\.[0-9]{1,3}){3}$' || \
+    printf '%s - WARNING: Tor proxy seems not to be working! Starting Boltz proxy anyway...\n' "${__B}"
+
 if [ -s "${__PIDFILE}" ]; then
   __PID=$(cat "${__PIDFILE}")
   if kill -0 "${__PID}" > /dev/null 2>&1; then
-    printf '%s - INFO: proxy is already running...\n' "${__B}"
+    printf '%s - INFO: Boltz proxy is already running...\n' "${__B}"
   else
     printf '%s - WARNING: pid file exists but process does not! Deleting pid file...\n' "${__B}"
     rm -f "${__PIDFILE}"
